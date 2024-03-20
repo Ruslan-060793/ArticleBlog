999a88f4aa2863893fd104ae1190e20b768eebb1
rp <rp@iclicks.io>
2024-03-20 03:16:25 +0100

    120

--- ArticleBlog/obj/Debug/net8.0/ArticleBlog.AssemblyInfo.cs
+++ ArticleBlog/obj/Debug/net8.0/ArticleBlog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArticleBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11a98acb4bbd9054009acc40c7612de9b90e26c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa6cc219faf37e6d1be851057be0846a3ae4084")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArticleBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArticleBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
